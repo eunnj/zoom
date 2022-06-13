84c8847698bf24a9241306710e548e722fe68c67
user <yenje7@naver.com>
2022-06-14 00:01:56 +0900

    Room Notifications

--- src/server.js
+++ src/server.js
@@ -23,6 +23,13 @@ wsServer.on("connection", (socket) => {
     done();
     socket.to(roomName).emit("welcome");
   });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
+  });
 });
 /*
 const wss = new WebSocket.Server({ server }); // ({server})=>같은 서버에서 http,webSpcket을 둘 다 작동
